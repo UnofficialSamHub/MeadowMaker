321e75768a199e734168ceac4dbc06e1f32ac481
Vuk <46850780+VukAnd@users.noreply.github.com>
2020-04-26 12:23:58 +0200

    add version number, disable resizing

--- MeadowLauncher/MeadowLauncher/MainForm.Designer.cs
+++ MeadowLauncher/MeadowLauncher/MainForm.Designer.cs
@@ -24,6 +24,7 @@
         /// </summary>
         private void InitializeComponent() {
             this.InstallBtn = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // InstallBtn
@@ -36,21 +37,38 @@
             this.InstallBtn.UseVisualStyleBackColor = true;
             this.InstallBtn.Click += new System.EventHandler(this.InstallBtn_Click);
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(751, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(37, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "v0.0.1";
+            // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.InstallBtn);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(816, 489);
+            this.MinimizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(816, 489);
             this.Name = "MainForm";
             this.Text = "MeadowLauncher";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Button InstallBtn;
+        private System.Windows.Forms.Label label1;
     }
 }
 
