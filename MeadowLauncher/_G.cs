0361bfe72cb349b4603307ddc7c3e0ae663f6515
Vuk <46850780+VukAnd@users.noreply.github.com>
2020-04-26 12:29:05 +0200

    swap to RHL

---
+++ MeadowLauncher/_G.cs
@@ -0,0 +1,10 @@
+﻿namespace ResourceHubLauncher
+{
+    class _G
+    {
+        public static bool dev => (bool)Config.Options["devmd"];
+        public static bool update => (bool)Config.Options["autoUpdate"];
+
+        public static bool beta => (bool)Config.Options["beta"];
+    }
+}
