0adda08d30b7aceeec7b86fc73f8b66e6ad7e955
Vuk (ghdesktop) <46850780+VukAnd@users.noreply.github.com>
2020-06-04 22:21:44 +0200

    Initial prep for MLL

---
+++ MeadowLandLauncher/MeadowLandLauncher/ActivationPanel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MeadowLandLauncher {
+    public partial class ActivationPanel : Form {
+        public ActivationPanel() {
+            InitializeComponent();
+        }
+
+        private void actBtn_Click(object sender, EventArgs e) {
+            if(ActivationBox.Text == "SHFP10402") {
+                this.Hide();
+                new MainForm().Show();
+            } else {
+                label1.Text = "Sorry, but that's an incorrect code! Nice try... :)";
+            }
+        }
+    }
+}
---
+++ MeadowLandLauncher/MeadowLandLauncher/MainForm.Designer.cs
@@ -0,0 +1,68 @@
+﻿namespace MeadowLandLauncher {
+    partial class MainForm {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if(disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.packList = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // packList
+            // 
+            this.packList.FormattingEnabled = true;
+            this.packList.Location = new System.Drawing.Point(12, 12);
+            this.packList.Name = "packList";
+            this.packList.Size = new System.Drawing.Size(120, 95);
+            this.packList.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(577, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(211, 39);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "MeadowLand Launcher Test\r\nDO NOT DISTRIBUTE!\r\nOnly for SamHub Friends Program mem" +
+    "bers";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.packList);
+            this.Name = "Form1";
+            this.Text = "MeadowLand Launcher";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox packList;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
