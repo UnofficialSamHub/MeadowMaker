7d9efe0abd9913832321cd64f9a103f1e6820ce0
Vukky <46850780+VukAnd@users.noreply.github.com>
2020-07-21 20:07:14 +0200

    label tweaks

--- src/MeadowLandMaker/PackGen.Designer.cs
+++ src/MeadowLandMaker/PackGen.Designer.cs
@@ -52,9 +52,9 @@
             this.label1.AutoSize = true;
             this.label1.Location = new System.Drawing.Point(4, 8);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(69, 13);
+            this.label1.Size = new System.Drawing.Size(112, 13);
             this.label1.TabIndex = 0;
-            this.label1.Text = "Sprite Sheet*";
+            this.label1.Text = "Sprite Sheet (required)";
             // 
             // SpriteSheetBox
             // 
@@ -77,9 +77,9 @@
             this.label2.AutoSize = true;
             this.label2.Location = new System.Drawing.Point(4, 55);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(58, 13);
+            this.label2.Size = new System.Drawing.Size(101, 13);
             this.label2.TabIndex = 2;
-            this.label2.Text = "Stationery*";
+            this.label2.Text = "Stationery (required)";
             // 
             // FontBox
             // 
@@ -94,9 +94,9 @@
             this.label3.AutoSize = true;
             this.label3.Location = new System.Drawing.Point(4, 102);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(32, 13);
+            this.label3.Size = new System.Drawing.Size(75, 13);
             this.label3.TabIndex = 4;
-            this.label3.Text = "Font*";
+            this.label3.Text = "Font (required)";
             // 
             // FontNameBox
             // 
@@ -110,9 +110,9 @@
             this.label4.AutoSize = true;
             this.label4.Location = new System.Drawing.Point(4, 148);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(63, 13);
+            this.label4.Size = new System.Drawing.Size(106, 13);
             this.label4.TabIndex = 6;
-            this.label4.Text = "Font Name*";
+            this.label4.Text = "Font Name (required)";
             // 
             // SpriteSheetDialog
             // 
@@ -188,9 +188,9 @@
             this.label5.AutoSize = true;
             this.label5.Location = new System.Drawing.Point(4, 192);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(63, 13);
+            this.label5.Size = new System.Drawing.Size(109, 13);
             this.label5.TabIndex = 13;
-            this.label5.Text = "Pack Name";
+            this.label5.Text = "Pack Name (optional)";
             // 
             // statusLabel
             // 
